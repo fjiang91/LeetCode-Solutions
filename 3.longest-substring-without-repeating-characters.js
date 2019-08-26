9db9113050919219383a7a00461fef66049264dc
Feng Jiang <fjiang91@gmail.com>
2019-08-26 17:17:53 -0400

    added 3.longest-substring-without-repeating-characters

--- 3.longest-substring-without-repeating-characters.js
+++ 3.longest-substring-without-repeating-characters.js
@@ -7,7 +7,25 @@
  * @param {string} s
  * @return {number}
  */
+//Two Pointer solution
+//Keep increasing the end pointer until we hit a duplicate char
+//Take the max of maxCount or (end - start)
 var lengthOfLongestSubstring = function(s) {
-    
-};
+  if(!s.length) return 0;
+  let foundMap = {};
+  let start = 0;
+  let end = 0;
+  let maxCount = -1;
+  while (end < s.length) {
+      if(!foundMap.hasOwnProperty(s[end])) {
+          foundMap[s[end]] = 1;
+          end++;
+          maxCount = Math.max(maxCount, end - start);
+      } else {
+          delete foundMap[s[start]];
+          start++;
+      }
+  };
 
+  return maxCount
+};
