a27f215e5d95121081325057e6379836d327c8f0
Feng Jiang <fjiang91@gmail.com>
2019-08-26 17:14:07 -0400

    added 3.longest-substring-without-repeating-characters

---
+++ 3.longest-substring-without-repeating-characters.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=3 lang=javascript
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    
+};
+
